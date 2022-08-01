0b3c56090bf77966964457a46c24b0938341009d
Marta Browarczyk <mart.br9@gmail.com>
2022-08-01 21:53:30 +0200

    initial version v1

--- app/src/main/java/pl/edu/pg/eti/segai/MainActivity.java
+++ app/src/main/java/pl/edu/pg/eti/segai/MainActivity.java
@@ -1,21 +1,93 @@
 package pl.edu.pg.eti.segai;
 
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.IOException;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    private GoogleDriveHandler googleDriveHandler;
+    private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    public void uploadPhoto(View view) throws IOException {
+        Intent imageCaptureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        imageUri = createImageUri();
+        imageCaptureIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        photoActivityResultLauncher.launch(imageCaptureIntent);
+    }
+
+    ActivityResultLauncher<Intent> photoActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    calculateAndReturnResult();
+                } else
+                    Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+            });
+
+
+    private void calculateAndReturnResult() {
+        //  TODO: AI MAGIC now we generate random values
+        Intent intent = new Intent(this, ResultActivity.class);
+        intent.putExtra("type", randomType());
+        intent.putExtra("probability", randomProbability());
+        intent.putExtra("imageUri", imageUri.toString());
+        startActivity(intent);
+    }
+
+
+    public void uploadPhotoFromDisc(View view) {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("*/*");
+        intent.setFlags((Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION));
+        uploadPhotoFromDiscActivityResultLauncher.launch(intent);
+    }
+
+
+    ActivityResultLauncher<Intent> uploadPhotoFromDiscActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        imageUri = data != null ? data.getData() : null;
+                        if (imageUri != null && imageUri.toString().contains("image"))
+                            calculateAndReturnResult();
+                        else
+                            Toast.makeText(MainActivity.this, "Failed. You didn't choose an image!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        imageUri = null;
+                        Toast.makeText(MainActivity.this, "Failed. You didn't choose any file!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     public void switchToDataCollecting(View view) {
@@ -38,9 +110,42 @@ public class MainActivity extends AppCompatActivity {
         adb.create().show();
     }
 
-    //TODO delte
-    public void resultView(View view) {
-        Intent intent = new Intent(this, ResultActivity.class);
-        startActivity(intent);
+
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    private Uri createImageUri() throws IOException {
+
+        long timeStamp = System.currentTimeMillis();
+        String imageFileName = "img_" + timeStamp;
+        // File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        // File image = File.createTempFile(imageFileName, Constants.EXTENSION_PHOTOS, storageDir);
+
+        Uri uri = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)
+            uri = MediaStore.Images.Media.getContentUri((MediaStore.VOLUME_EXTERNAL_PRIMARY));
+        else
+            uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+
+        ContentResolver resolver = getContentResolver();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, imageFileName + Constants.EXTENSION_PHOTOS);
+        contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, Constants.PATH_PHOTOS);
+        return resolver.insert(uri, contentValues);
+    }
+
+    //TODO: delete randomizing methods
+    private Double randomProbability() {
+        double probability = 0.01 + Math.random() * (99.99 - 0.01);
+        return Math.round(probability * 100) / 100.0;
+    }
+
+    private String randomType() {
+        String[] results = {
+                "BIO",
+                "PLASTIC/METAL",
+                "PAPER",
+                "GLASS",
+                "MIXED"};
+        Random r = new Random();
+        return results[r.nextInt(results.length)];
     }
 }
--- app/src/main/java/pl/edu/pg/eti/segai/ResultActivity.java
+++ app/src/main/java/pl/edu/pg/eti/segai/ResultActivity.java
@@ -3,20 +3,25 @@ package pl.edu.pg.eti.segai;
 import static java.lang.Character.toChars;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.Random;
-
 public class ResultActivity extends AppCompatActivity {
 
     TextView resultInfo;
+    String type;
+    Double probability;
+    ImageView imageView;
+    Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,34 +30,26 @@ public class ResultActivity extends AppCompatActivity {
         resultInfo = findViewById(R.id.textViewResult);
         Button backButton = findViewById(R.id.backButton);
         backButton.setText(new String(toChars(0x1F519)));
+        ImageView imageView = findViewById(R.id.imageView);
         resultInfo.setVisibility(View.INVISIBLE);
-        (new Handler()).postDelayed(this::showRandomResultInfo, 1000);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            type = extras.getString("type");
+            probability = extras.getDouble("probability");
+            imageUri = Uri.parse(extras.getString("imageUri"));
+        }
+        imageView.setImageURI(imageUri);
+
+        (new Handler()).postDelayed(this::showResultInfo, 1000);
     }
 
-    private void showRandomResultInfo() {
-        showResultInfo(randomType(), randomProbability());
+    private void showResultInfo() {
+        showResultInfo(type, probability);
     }
-
 
     private void showResultInfo(String type, Double probability) {
         resultInfo.setText(String.format("Your trash is %s at %s%%", type, probability));
         resultInfo.setVisibility(View.VISIBLE);
-    }
-
-    private Double randomProbability() {
-        double probability = 0.01 + Math.random() * (99.99 - 0.01);
-        return Math.round(probability * 100) / 100.0;
-    }
-
-    private String randomType() {
-        String[] results = {
-                "BIO",
-                "PLASTIC/METAL",
-                "PAPER",
-                "GLASS",
-                "MIXED"};
-        Random r = new Random();
-        return results[r.nextInt(results.length)];
     }
 
     @Override
@@ -65,10 +62,10 @@ public class ResultActivity extends AppCompatActivity {
         adb.setTitle("Can we save your photo to improve our model?");
         adb.setPositiveButton("YES",
                 (dialog, which) -> {
-                    //TODO: SAVE PHOTO with result!!!
+                    //TODO: SAVE PHOTO with category!!!
                     Intent intent = new Intent(this, MainActivity.class);
                     startActivity(intent);
-                    //TODO: maybe add some "thank you" pop up
+                    Toast.makeText(ResultActivity.this, "Thanks!", Toast.LENGTH_SHORT).show();
                 });
         adb.setNegativeButton("NO",
                 (dialog, which) -> {
